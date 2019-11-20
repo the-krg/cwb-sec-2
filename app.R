c308f3d55d02060719f4aa161aeadd6fab80e336
Daniel Machado <danielmachadopintos@gmail.com>
2019-11-20 01:15:03 -0300

    feat(config): more config
    More

--- app.R
+++ app.R
@@ -13,6 +13,10 @@ n2017 <- as.data.frame(table(ds[ds$OCORRENCIA_ANO == 2017, "OCORRENCIA_MES"]))
 n2018 <- as.data.frame(table(ds[ds$OCORRENCIA_ANO == 2018, "OCORRENCIA_MES"]))
 n2019 <- as.data.frame(table(ds[ds$OCORRENCIA_ANO == 2019, "OCORRENCIA_MES"]))
 
+# add 0 to remaining months
+n2019 <- rbind(n2019, data.frame(Var1="11", Freq=NA))
+n2019 <- rbind(n2019, data.frame(Var1="12", Freq=NA))
+
 # dia
 ocorrencia_por_semana <- as.data.frame(table(ds$OCORRENCIA_DIA_SEMANA))
 
@@ -26,7 +30,7 @@ colnames(n2019)[2] = 'Freq2019'
 aux <- merge(n2015,n2016)
 aux <- merge(aux, n2017)
 aux <- merge(aux, n2018)
-# aux <- merge(aux, n2019)
+aux <- merge(aux, n2019)
 
 aux <- aux[order(aux$Var1),]
 
@@ -58,10 +62,11 @@ server <- function(input, output) {
     
     
     output$linePerYear <- renderPlotly({
-        plot_ly(aux, x = ~Var1, y = ~Freq2015, type='scatter', mode='lines')%>%
+        plot_ly(aux, x = ~Var1, y = ~Freq2015, name='2015', type='scatter', mode='lines')%>%
             add_trace(y = ~Freq2016, name = '2016', line = list(color = 'rgb(201)', width = 4)) %>%
             add_trace(y = ~Freq2017, name = '2017', line = list(color = 'rgb(22, 96, 167)', width = 4)) %>%
             add_trace(y = ~Freq2018, name = '2018', line = list(color = 'rgb(22, 96, 167)', width = 4)) %>%
+            add_trace(y = ~Freq2019, name = '2019', line = list(color = 'rgb(22, 96, 167)', width = 4)) %>%
             layout(title = 'Número de Ocorrencias por Ano')
     })
     
